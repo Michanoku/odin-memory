f63e942eef6bcfd6526a8d928a5a99bace3a5aed
Michanoku <michael.werker@gmail.com>
2026-03-30 01:11:28 +0000

    Feat: Initial card functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react';
-import ScoreBoard from './components/ScoreBoard'
-import GameBoard from './components/GameBoard'
+import ScoreBoard from './components/ScoreBoard.jsx'
+import GameBoard from './components/GameBoard.jsx'
 import titleImg from './assets/title.png';
 import './styles/index.css'
+import './styles/GameBoard.css'
+
+const numberOfCards = 12;
 
 function App() {
   const [highScore, setHighScore] = useState(0);
@@ -34,7 +37,7 @@ function App() {
       <div className="explanation"></div>
     </header>
     <main>
-      <GameBoard gameState={gameState} onReady={startGame}/>
+      <GameBoard gameState={gameState} numberOfCards={numberOfCards} onReady={startGame}/>
     </main>
     </>
   )
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,9 +1,81 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from "react";
 
-export default function GameBoard() {
-    const [cardList, setCardList] = useState([]);
+function Card({id, name, cry, sprite, types}) {
 
+  return (
+    <div className="card">
+      <img src={sprite}></img>
+    </div>
+  )
+}
 
+// The gameboard that handles card creation and such 
+export default function GameBoard({ gameState, numberOfCards, onReady }) {
+  const [pokeList, setPokeList] = useState([]);
 
-    return <div>Gameboard</div>
-};
+  // This is used to only fetch from the API once even in DEV with strict mode
+  const hasFetched = useRef(false)
+  useEffect(() => {
+    // If we loaded, return
+    if (!hasFetched.current) {
+      hasFetched.current = true;
+      fetchPokemon();
+    }
+    // The linter does not like this empty array, but i dont want to silence it for now
+  }, []);
+
+  async function fetchPokemon() {
+    const idList = createIdList();
+    const promises = idList.map(async pokeId => {
+      try {
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${pokeId}`,
+        );
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+        }
+        const result = await response.json();
+        return createPokeData(result);
+      } catch (error) {
+        console.error(error.message);
+      }
+    })
+    const results = await Promise.all(promises);
+    setPokeList(results);
+    onReady();
+  }
+
+  // Make a list of Pokemon IDs to fetch from the API
+  function createIdList() {
+    let idList = [];
+    while (idList.length < numberOfCards) {
+      const newId = Math.floor(Math.random() * 151 + 1);
+      if (!idList.includes(newId)) {
+        idList.push(newId);
+      }
+    }
+    return idList;
+  }
+
+  function createPokeData(data) {
+    const pokemon = {
+      id: data.id,
+      name: data.name,
+      cry: data.cries.latest,
+      sprite: data.sprites.front_default,
+      types: [],
+    };
+    for (const pokeType of data.types) {
+      pokemon.types.push(pokeType.type.name);
+    }
+    return pokemon;
+  }
+
+  return (
+    <div className="gameBoard">
+      {pokeList.map(pokemon => 
+        <Card key={pokemon.id} id={pokemon.id} name={pokemon.name} cry={pokemon.cry} sprite={pokemon.sprite} types={pokemon.types}/>
+      )}
+    </div>
+  )
+}
